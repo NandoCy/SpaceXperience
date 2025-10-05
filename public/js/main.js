3bd601937c4a2864d011a37b52dce7cb78f218cf
NandoCy <146496666+NandoCy@users.noreply.github.com>
2025-10-05 10:50:42 -0600

    Delete public directory

--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-const btnMenu = document.getElementById("btn-menu");
-const menu = document.querySelector("aside");
-btnMenu.addEventListener("click", () => {
-  const visible = menu.style.transform === "translateX(0%)";
-  menu.style.transform = visible ? "translateX(100%)" : "translateX(0%)";
-});
